ae629e5247a9218c8f54fd7b04a5e2a87e110822
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-04-08 06:00:14 +0530

    auth middleware

---
+++ frontend/utils/middleware.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { auth } from "../src/firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+export default function AuthMiddleware({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/login");
+      } else {
+        setLoading(false);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="flex items-center gap-3">
+          <div className="animate-spin rounded-full h-6 w-6 border-2 border-white border-t-transparent"></div>
+          <p className="text-2xl">Loading...</p>
+        </div>
+      </div>
+    );
+  }
+  return <>{children}</>;
+}
